9fd80435ea3145f18327baad2362822d79a6121d
Vicky91661 <kumar.vickysah72@gmail.com>
2024-01-23 16:46:57 +0530

    Added zod input Authentication to signin and signup , also added bcrypt to store hashed password to database

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,37 +4,60 @@ const {User} = require("../db")
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require("../config");
 
+const {z} = require("zod")
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const router = express.Router();
+
+const userSignin =z.object({
+    username:z.string().email().min(3, { message: "username must be 3 or more characters long" })
+    .max(30,{message:"username must be 30 or less characters long"}),
+    password:z.string().min(5,{message:"password must be more than 5 length"})
+})
+const userSignup =z.object({
+    username:z.string().email().min(3, { message: "username must be 3 or more characters long" })
+    .max(30,{message:"username must be 30 or less characters long"}),
+    password:z.string().min(5,{message:"password must be more than 5 length"}),
+    firstName:z.string().max(50, { message: "firstName must be 50 or less characters long" }),
+    lastName:z.string().max(50, { message: "lastName must be 50 or less characters long" })
+})
 
 router.post("/signin",async (req,res)=>{
     const username =req.body.username
     const password =req.body.password
     
+    try {
+        userSignin.parse({username,password})
+        const userExist = await User.findOne({username});
 
-    const userExist = await User.findOne({username});
+        if(userExist){
+            const result = await bcrypt.compare(password,userExist.password)
+            if(result){
+                const newUser = userExist._id ;
+                var token = jwt.sign({ newUser }, JWT_SECRET);
+                res.status(200).json({
+                    message: "successfully login",
+                    token
+                })
+            }else{
+                res.status(411).json({
+                    message: "Password is incorrect"
+                })
+            }
+            
 
-    if(userExist){
-
-
-        if(userExist.password===password){
-            const newUser = userExist._id ;
-            var token = jwt.sign({ newUser }, JWT_SECRET);
-            res.status(200).json({
-                message: "successfully login",
-                token
-            })
         }else{
             res.status(411).json({
-                message: "Password is incorrect"
+                message: "User does not exist"
             })
         }
-        
-
-    }else{
+    } catch (error) {
         res.status(411).json({
-            message: "User does not exist"
-        })
+            message: "Invalid input",
+            errors: error.errors.map(err => err.message),
+        });
     }
 
 })
@@ -47,34 +70,48 @@ router.post("/signup",async (req,res)=>{
     const firstName =req.body.firstName
     const lastName =req.body.lastName
 
-    const userExist = await User.findOne({username});
+    try {
+        userSignup.parse({ username, password, firstName, lastName });
+        
+        const userExist = await User.findOne({username});
 
-    if(userExist){
-        res.status(411).json({
-            message: "Email already taken / Incorrect inputs"
-        })
-
-    }else{
-        const user = await User.create({
-            username,
-            password,
-            firstName,
-            lastName,
-        })
-        if(user){
-            const newUser = user._id ;
-            var token = jwt.sign({ newUser }, JWT_SECRET);
-            res.status(200).json({
-                message: "User created successfully",
-                token
-            })
-        }else{
+        if(userExist){
             res.status(411).json({
-                message: "not inserted into database"
+                message: "Email already taken"
             })
+
+        }else{
+            const hashedPassword = await bcrypt.hash(password, saltRounds);
+            const user = await User.create({
+                username,
+                password:hashedPassword,
+                firstName,
+                lastName,
+            })
+            if(user){
+                const newUser = user._id ;
+                var token = jwt.sign({ newUser }, JWT_SECRET);
+                res.status(200).json({
+                    message: "User created successfully",
+                    token
+                })
+            }else{
+                res.status(411).json({
+                    message: "not inserted into database"
+                })
+            }
+        
         }
-       
+    } catch (error) {
+        res.status(411).json({
+            message: "Invalid input",
+            errors: error.errors.map(err => err.message),
+        });
     }
+
+    
+    
+        
     
 })
 router.post("/changePassword",(req,res)=>{
